aad1140f0c06f8a363e29420abe6f78241536117
HershPatel7 <94482574+HershPatel7@users.noreply.github.com>
2021-12-08 10:35:34 -0500

    Update CarRental_GUI.java

--- CarRental_GUI.java
+++ CarRental_GUI.java
@@ -1,6 +1,6 @@
+// Written by Hersh, Dylan, and Chris
 package __CarRentalApp;
 import java.util.ArrayList;
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
